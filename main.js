b680c87e59dceef206a23eaa42d1089c958735b2
Gopala-kri <138243972+Gopala-kri@users.noreply.github.com>
2023-09-14 22:31:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,65 +1,18 @@
+var a= num1;
 
-        
-        const items = [
-            { name: "Product 1", price: 10.99 },
-            { name: "Product 2", price: 5.99 },
-            { name: "Product 3", price: 8.49 }
-        ];
+var b= num2;
 
+function performAddition() {
 
-        function calculateTotal() {
-            const cartItems = document.getElementById("cart-items");
-            const totalAmount = document.getElementById("total-amount");
-            const printButton = document.querySelector(".print-button");
+parseFloat(document.getElementById("a").value); parseFloat (document.getElementById("b").value);
 
-            let total = 0;
+var sum = a + b;
 
-            cartItems.innerHTML = "";
+var audio = document.getElementById("audio");
 
-    
-            items.forEach((item, index) => {
-                const row = document.createElement("tr");
-                const itemName = document.createElement("td");
-                const itemPrice = document.createElement("td");
-                const itemQuantity = document.createElement("td");
-                const itemTotal = document.createElement("td");
+audio.play();
 
-                itemName.textContent = item.name;
-                itemPrice.textContent = item.price.toFixed(2);
-                const quantityInput = document.createElement("input");
-                quantityInput.type = "number";
-                quantityInput.min = 0;
-                quantityInput.value = 0;
-                quantityInput.id = `quantity-${index}`;
-                itemQuantity.appendChild(quantityInput);
+document.getElementById("result").textContent = "The result of the addition is:
 
-                row.appendChild(itemName);
-                row.appendChild(itemPrice);
-                row.appendChild(itemQuantity);
-                row.appendChild(itemTotal);
-
-                cartItems.appendChild(row);
-
-                quantityInput.addEventListener("input", () => {
-                    const quantity = parseInt(quantityInput.value) || 0;
-                    const itemTotalValue = (item.price * quantity).toFixed(2);
-                    itemTotal.textContent = itemTotalValue;
-
-                   
-                    calculateTotal();
-                });
-            });
-
-            
-            const itemTotals = document.querySelectorAll("td:nth-child(4)");
-            itemTotals.forEach((itemTotal) => {
-                total += parseFloat(itemTotal.textContent);
-            });
-
-          
-            totalAmount.textContent = total.toFixed(2);
-
-        
-            printButton.style.display = "block";
-        }
-   
++ result;
+}
